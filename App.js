8d69a1aa18de80bf0ebec33b029bf3f948e1ee53
Shahidul Islam <shahidulislam7150@gmail.com>
2022-07-17 00:19:53 +0600

    native home screen designing

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Home from './src/screens/Home';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum"
+  return <View style={styles.mainContainer}>
+    <View style={styles.homeTop}>
+      <Image style={styles.headerImage} resizeMode="contain" source={require("./assets/screentab.jpg")} />
+      {/* <Image style={styles.headerImage} resizeMode="contain" source={require("./assets/logo.jpeg")} /> */}
+      <Text style={styles.mainHeader} >Welcome to</Text>
+      <Text style={[styles.mainHeader, {
+        fontSize: 33,
+        color: "#4c5dab",
+        marginTop: 0,
+      }]} >Bit technical</Text>
+      <Text style={styles.paraStyle}>{description}</Text>
     </View>
-  );
+  </View>
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  mainContainer: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    textAlign: "center"
   },
+  homeTop: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 10,
+
+  },
+  headerImage: {
+    height: undefined,
+    width: "100%",
+    aspectRatio: 1,
+    display: "flex",
+    alignItems: 'stretch',
+    marginTop: 50,
+    borderRadius: 20,
+
+  },
+  mainHeader:{
+    fontSize:28,
+    fontWeight:"700",
+    color:"#344055",
+    textTransform:"uppercase",
+    lineHeight:48,
+    marginTop:50
+  }
 });
